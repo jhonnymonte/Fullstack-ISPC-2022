435477238a1ee36d708842f5037b0163bdbfccea
desimingo <desimingo15@gmail.com>
2023-05-21 02:05:15 -0300

    modificacion componente list

--- ProgramadorWeb2023/ConnetSalud/src/app/components/list-citas/list-citas.component.ts
+++ ProgramadorWeb2023/ConnetSalud/src/app/components/list-citas/list-citas.component.ts
@@ -1,26 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
+
 @Component({
-selector: 'app-list-citas',
-templateUrl: './list-citas.component.html',
-styleUrls: ['./list-citas.component.css']
+  selector: 'app-list-citas',
+  templateUrl: './list-citas.component.html',
+  styleUrls: ['./list-citas.component.css']
 })
+
 export class ListCitasComponent implements OnInit {
-users:any;
-constructor(private dataservice: DataService) {}
-ngOnInit(): void {
-this.fetchUser();
-}
-deleteCitas(id_paciente:any){
-console.log(id_paciente);
-this.dataservice.delCitas(id_paciente).subscribe(()=>{
-this.fetchUser();
-})
-}
-fetchUser(){
-this.dataservice.listCitas().subscribe((data)=> {
-this.users=data;
-console.log(this.users);
-});
-}
+  users:any;
+  
+  constructor(private dataservice: DataService) {}
+  
+  ngOnInit(): void {
+    this.fetchUser();
+  }
+  
+  deleteCitas(id_paciente:any){
+    console.log(id_paciente);
+    this.dataservice.delCitas(id_paciente).subscribe(()=>{
+    this.fetchUser();
+  })
+  }
+  
+  fetchUser(){
+    this.dataservice.listCitas().subscribe((data)=> {
+    this.users=data;
+    console.log(this.users);
+  });
+  }
 }
