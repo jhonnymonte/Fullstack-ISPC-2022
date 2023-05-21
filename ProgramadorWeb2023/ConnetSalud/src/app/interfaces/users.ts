399a1fd900d03dc536798955202c66c47fc3a7f2
ediberto <coronelvediberto@gmail.com>
2023-05-21 01:58:45 -0300

    nueva interface

---
+++ ProgramadorWeb2023/ConnetSalud/src/app/interfaces/users.ts
@@ -0,0 +1,11 @@
+export interface Users{
+    id_paciente: number;
+    dni: number;
+    nombre: string;
+    apellido: string;
+    telefono: string;
+    correo: string;
+    fecha: string;
+    hora: string;
+    mensaje: string;
+}
